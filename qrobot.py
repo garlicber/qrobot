86ac258c8d38f9db167da0cfab0637d98bab213c
jk <ulukaiz@hotmail.com>
2014-03-03 21:27:42 +0100

    added field type to state

--- qrobot.py
+++ qrobot.py
@@ -14,14 +14,18 @@ FIELD_ENEMY = 1 << 4
 FIELD_OBSTACLE = 1 << 1
 
 def hp2discrete(hp):
-    return HP_HIGH
-
+    if 50 > hp >40:
+        return HP_HIGH
+    if 40> hp > 15:
+        return HP_MEDIUM
+    else:
+        return HP_LOW
 
 class Field:
-    def __init__(self, x, y, player_id=0, hp=0, tpe=FIELD_NORMAL):
+    def __init__(self, player_id=0, hp=0, tpe=FIELD_NORMAL):
         self.player_id = player_id
         self.hp = 0
-        self.type = tpe
+        self.type = type
 
     def set_robot(self, robot):
         self.hp = hp2discrete(robot.hp)
@@ -31,15 +35,8 @@ class Field:
         else:
             self.type = FIELD_ENEMY
 
-        loc_types = rg.loc_types(robot.location)
-        if "invalid" in loc_types:
-            self.type = FIELD_OBSTACLE
-        elif "normal" in loc_types:
-            self.type = FIELD_NORMAL
-        elif "obstacle" in loc_types:
-            self.type = FIELD_OBSTACLE
-        elif "spawn" in loc_types:
-            self.type = FIELD_SPAWN
+    def set_location_type(self, loc):
+        self.type = rg.loc_types(loc);
 
 class State:
     def __init__(self, hp, fields):
@@ -63,7 +60,7 @@ class State:
             robot_loc = (loc_x + fx, loc_y + fy)
             if robot_loc in game.robots:
                 state.fields[(fx, fy)].set_robot(game.robots[robot_loc])
-
+            state.fields[(fx, fy)].set_location_type(robot_loc)
         state.hp = robot.hp
         return state
 
@@ -81,42 +78,12 @@ class State:
                     print "[ ]",
                 print ""
 
+
 class QTable:
     def __init__(self):
         self.states = self._generate_all_states()
         self.actions = self._actions()
         self.Q = {}
-
-    def _generate_all_states(self):
-        fields = []
-        for x in range(-SIGHT, SIGHT+1):
-            for y in range(-SIGHT, SIGHT+1):
-                if abs(x)+abs(y) <= SIGHT and (x != 0 and y != 0):
-                    fields.append(Field(x,y))
-
-
-        for f in fields:
-            f.player_id = {1, -1}
-
-        totalFields = 24
-        permutString = []
-
-        for enemies in range (0, totalFields + 1):
-            fieldsLeftFriends = totalFields - enemies
-            for friends in range (0, fieldsLeftFriends + 1):
-                fieldsLeftNeutral = fieldsLeftFriends - friends
-                for neutral in range (0, fieldsLeftNeutral + 1):
-                    fieldsLeftBlocked = fieldsLeftNeutral - neutral
-                    for blocked in range(0, fieldsLeftBlocked):
-                        stateString = 'E'* enemies + 'F' * friends + 'N' * neutral + 'B' * blocked
-                        for state in itertools.permutations(stateString):
-                            permutString.append(state)
-        print(len(permutString))
-
-        fields.append()
-
-
-
 
     def _actions(self):
         return [["suicide"]]
