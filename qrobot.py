1a190525324662356c6d8e30bf26b8c700c9be41
berleon <github@leon-sixt.de>
2014-03-03 22:31:30 +0100

    fixed predict

--- qrobot.py
+++ qrobot.py
@@ -1,4 +1,3 @@
-import itertools
 import rgkit.rg as rg
 
 SIGHT = 3
@@ -11,12 +10,16 @@ FIELD_NORMAL = 1 << 1
 FIELD_FRIEND = 1 << 2
 FIELD_SPAWN = 1 << 3
 FIELD_ENEMY = 1 << 4
-FIELD_OBSTACLE = 1 << 1
+FIELD_OBSTACLE = 1 << 5
+
+ACTION_SUICIDE = 1
+ACTION_MOVE = 1
+ACTION_ATTACK = 1
 
 def hp2discrete(hp):
-    if 50 > hp >40:
+    if 50 > hp > 40:
         return HP_HIGH
-    if 40> hp > 15:
+    if 40 > hp > 15:
         return HP_MEDIUM
     else:
         return HP_LOW
@@ -36,7 +39,15 @@ class Field:
             self.type = FIELD_ENEMY
 
     def set_location_type(self, loc):
-        self.type = rg.loc_types(loc);
+        loc_types = rg.loc_types(loc)
+        if "invalid" in loc_types:
+            self.type = FIELD_OBSTACLE
+        elif "normal" in loc_types:
+            self.type = FIELD_NORMAL
+        elif "obstacle" in loc_types:
+            self.type = FIELD_OBSTACLE
+        elif "spawn" in loc_types:
+            self.type = FIELD_SPAWN
 
 class State:
     def __init__(self, hp, fields):
@@ -93,38 +104,59 @@ class QTable:
         else:
             return self.DEFAULT_REWARD
 
-
+    def setQ(self, state, action, reward):
+        self.q[(state, action)] = reward
 
     @staticmethod
     def _actions():
-        actions = [["suicide"]]
+        actions = [ACTION_SUICIDE]
         for x in [-1, 1]:
             for y in [-1, 1]:
-                actions.append(["move", (x, y)])
-                actions.append(["attack", (x, y)])
+                actions.append((ACTION_ATTACK, (x, y)))
+                actions.append((ACTION_MOVE, (x, y)))
         return actions
 
-    def predict(self, robot):
-        state = State.from_game(robot)
-        action = max([self.getQ(state, a) for a in self.actions])
+    def predict(self, state):
+        action = max(self.actions, key=lambda a: self.getQ(state, a))
         return action
 
-    def learn(self, robot):
+    def learn(self, state, action, new_state):
         return
 
+    @staticmethod
+    def map_action(action, loc):
+        if action == ACTION_SUICIDE:
+            return ["suicide"]
+
+        (action_code,(rel_x, rel_y)) = action
+        (loc_x, loc_y) = loc
+        (abs_x, abs_y) = (rel_x + loc_x, rel_y + loc_y)
+        if action_code == ACTION_ATTACK:
+            return ["attack", (abs_x, abs_y)]
+        if action_code == ACTION_MOVE:
+            return ["move", (abs_x, abs_y)]
+
+        return "error"
 
 class Robot:
     game = None
+    last_game = None
+
     qlearning = QTable()
-    last_actions = {}
+    last_action = {}
+    last_state = {}
 
     def act(self, game):
-        if self.robot_id not in self.last_actions:
-            self.last_actions[self.robot_id] = []
-
         self.game = game
-      #  action = self.qlearning.predict(self)
-      #  self.last_actions[self.robot_id].append(action)
-      #  self.qlearning.learn()
-      #  return action
-        return ["guard"]
+        new_robot = self.robot_id not in self.last_action
+
+        state = State.from_game(self)
+        action = self.qlearning.predict(state)
+
+        if not new_robot:
+            self.qlearning.learn(self.last_state[self.robot_id], self.last_action[self.robot_id], state)
+
+        self.last_state[self.robot_id] = state
+        self.last_action[self.robot_id] = action
+        self.last_game = game
+        return QTable.map_action(action, self.location)
